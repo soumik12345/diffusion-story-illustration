917ced31906c8523adffaf68172b3ad9c613f350
soumik12345 <19soumik.rakshit96@gmail.com>
2024-08-23 22:24:49 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="story_illustrator",
+    version="0.1.0",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/soumik12345/diffusion-story-illustration",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+    install_requires=open("requirements.txt").read().splitlines(),
+)
